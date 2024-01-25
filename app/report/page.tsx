3f9fdf3dbd51865bc4eaa4a711e94ddc55ce7a51
Sevasit <new12253952@gmail.com>
2024-01-25 19:29:51 +0700

    fix disable 2

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -278,8 +278,9 @@ export default function Report() {
                     <span>ยกเลิก</span>
                   </div>
                 </Link>
-                {!disableSubmit ? (
+                {disableSubmit ? (
                   <button
+                    disabled
                     className={`cursor-not-allowed w-20 bg-white border-2 border-[#0f8d67] text-[#0f8d67] hover:bg-[#00DC82] hover:border-black hover:text-white duration-300 shadow-md rounded-lg flex gap-1 justify-between px-4 items-center`}
                   >
                     <span>ยืนยัน</span>
